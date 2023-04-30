940bcecbc51c659c968e5ed3757521b15642ba3e
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2023-04-30 16:47:04 -0300

    dart

---
+++ Dart/VariaveisInter.dart
@@ -0,0 +1,31 @@
+void main(){
+    var primeiraVariavel = "Ola Mundo";
+    print(primeiraVariavel);
+    
+    //verdadeiro
+    print ( 0 == 0);
+
+    int saldoDaConta = 100;
+    print(saldoDaConta);    
+
+    //Boas prátricas para instanciar inteiro
+    var z = 100;
+    //sera instanciada depois
+    late int b;
+
+    var a= 5;
+    //adicionar mais um
+    a ++ ;
+    print(a);
+
+
+    print("Contando até 10 em dart com for");
+
+    for(var number = 1 ; 10 >= number;number++ ){
+        print(number);
+    }
+
+
+
+
+}
