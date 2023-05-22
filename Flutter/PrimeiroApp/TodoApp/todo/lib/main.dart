0bdd6910fb8aca1a5836d1d93bd301724938555b
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2023-05-22 07:53:18 -0300

    Statefull

--- Flutter/PrimeiroApp/TodoApp/todo/lib/main.dart
+++ Flutter/PrimeiroApp/TodoApp/todo/lib/main.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(
-    child: Center(
-      child: Text(
-        "Ola Mundo",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 40.0),
-      ),    
-    ),
-  ));
+  runApp(AppWidget());
 }
 
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: ThemeData.light(), home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  final title = "Ola Mundo!";
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+          child: GestureDetector(
+        child: Text("Counter: $counter"),
+        onTap: () {
+          counter++;
+        },
+      )),
+    );
+  }
+}
