0a040113f89ecae0a1d77cb1e0f9c326287d328e
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2023-05-24 07:05:58 -0300

    aulas

---
+++ Flutter/Aulas/aulas/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(AulasApp());
+}
+
+class AulasApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
